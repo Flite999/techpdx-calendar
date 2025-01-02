84b0b0659c34a9680e848d33fc50be478f8d288d
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2025-01-02 07:59:28 -0800

    update seed to run in prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,53 +10,51 @@
 
 require 'faker'
 
-if Rails.env.development? || Rails.env.test?
-    2.times do
-        Event.create!(
-            title: Faker::Lorem.sentence,
-            start_time: Faker::Time.between(from: DateTime.now.beginning_of_day, to: DateTime.now.end_of_day),
-            end_time: Faker::Time.between(from: DateTime.now.beginning_of_day, to: DateTime.now.end_of_day),
-            google_map_link: Faker::Internet.url,
-            website: Faker::Internet.url,
-            description: Faker::Lorem.paragraph,
-            venue_details: Faker::Address.full_address,
-            tags: Faker::Lorem.words(number: 4).join(", ")
-        )
-    end
-    2.times do
-        Event.create!(
-            title: Faker::Lorem.sentence,
-            start_time: Faker::Time.forward(days: 1, period: :morning),
-            end_time: Faker::Time.forward(days: 1, period: :evening),
-            google_map_link: Faker::Internet.url,
-            website: Faker::Internet.url,
-            description: Faker::Lorem.paragraph,
-            venue_details: Faker::Address.full_address,
-            tags: Faker::Lorem.words(number: 4).join(", ")
-        )
-    end
-    2.times do
-        Event.create!(
-            title: Faker::Lorem.sentence,
-            start_time: Faker::Time.forward(days: 5, period: :morning),
-            end_time: Faker::Time.forward(days: 5, period: :evening),
-            google_map_link: Faker::Internet.url,
-            website: Faker::Internet.url,
-            description: Faker::Lorem.paragraph,
-            venue_details: Faker::Address.full_address,
-            tags: Faker::Lorem.words(number: 4).join(", ")
-        )
-    end
-    2.times do
-        Event.create!(
-            title: Faker::Lorem.sentence,
-            start_time: Faker::Time.forward(days: 12, period: :morning),
-            end_time: Faker::Time.forward(days: 12, period: :evening),
-            google_map_link: Faker::Internet.url,
-            website: Faker::Internet.url,
-            description: Faker::Lorem.paragraph,
-            venue_details: Faker::Address.full_address,
-            tags: Faker::Lorem.words(number: 4).join(", ")
-        )
-    end
+2.times do
+    Event.create!(
+        title: Faker::Lorem.sentence,
+        start_time: Faker::Time.between(from: DateTime.now.beginning_of_day, to: DateTime.now.end_of_day),
+        end_time: Faker::Time.between(from: DateTime.now.beginning_of_day, to: DateTime.now.end_of_day),
+        google_map_link: Faker::Internet.url,
+        website: Faker::Internet.url,
+        description: Faker::Lorem.paragraph,
+        venue_details: Faker::Address.full_address,
+        tags: Faker::Lorem.words(number: 4).join(", ")
+    )
+end
+2.times do
+    Event.create!(
+        title: Faker::Lorem.sentence,
+        start_time: Faker::Time.forward(days: 1, period: :morning),
+        end_time: Faker::Time.forward(days: 1, period: :evening),
+        google_map_link: Faker::Internet.url,
+        website: Faker::Internet.url,
+        description: Faker::Lorem.paragraph,
+        venue_details: Faker::Address.full_address,
+        tags: Faker::Lorem.words(number: 4).join(", ")
+    )
+end
+2.times do
+    Event.create!(
+        title: Faker::Lorem.sentence,
+        start_time: Faker::Time.forward(days: 5, period: :morning),
+        end_time: Faker::Time.forward(days: 5, period: :evening),
+        google_map_link: Faker::Internet.url,
+        website: Faker::Internet.url,
+        description: Faker::Lorem.paragraph,
+        venue_details: Faker::Address.full_address,
+        tags: Faker::Lorem.words(number: 4).join(", ")
+    )
+end
+2.times do
+    Event.create!(
+        title: Faker::Lorem.sentence,
+        start_time: Faker::Time.forward(days: 12, period: :morning),
+        end_time: Faker::Time.forward(days: 12, period: :evening),
+        google_map_link: Faker::Internet.url,
+        website: Faker::Internet.url,
+        description: Faker::Lorem.paragraph,
+        venue_details: Faker::Address.full_address,
+        tags: Faker::Lorem.words(number: 4).join(", ")
+    )
 end
